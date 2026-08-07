034b022d4d620ab659420587bed531380e735557
aymarhq <contatos.aymar@gmail.com>
2026-08-07 11:58:48 -0300

    Atualiza numero do WhatsApp

--- lib/site.ts
+++ lib/site.ts
@@ -1,4 +1,4 @@
-export const WHATSAPP_NUMBER = "5598985190160";
+export const WHATSAPP_NUMBER = "5598920064382";
 export const WHATSAPP_MSG = "Oi! Vim pelo site da Aymar. Queria falar sobre a operação da minha empresa.";
 export const WHATSAPP_URL =
   `https://wa.me/${WHATSAPP_NUMBER}?text=${encodeURIComponent(WHATSAPP_MSG)}`;
